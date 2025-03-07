abc000617079e47f2b5b49b121bc64ece31d9de4
sspirig <santiago.sprg@eduge.ch>
2025-03-07 16:01:43 +0100

    Added genially

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'chatbot_db';
-$username = 'zackaryist'; 
-$password = 'Super';
+$username = 'santiago.sprg'; 
+$password = 'SuperKey1';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, [
