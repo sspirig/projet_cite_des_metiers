ea745667ba62d5f93fdb2a33d2ff0bdb3e1d8249
Zacklemakak <zackary.ist@eduge.ch>
2025-04-04 12:47:47 +0200

    Restructuration des fichiers

--- php/db.php
+++ php/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $dbname = 'chatbot_db';
-$username = ''; 
-$password = '';
+$username = 'zackaryist'; 
+$password = 'Super';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, [
